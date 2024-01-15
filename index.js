44f30de37c41ffe99f572adb818d888eaed09d82
seasons986 <annalin12345@gmail.com>
2024-01-15 22:18:10 +0800

    theyve become trees <3

--- index.js
+++ index.js
@@ -13,24 +13,42 @@ window.addEventListener("load", () => {
   // 4) fire 灬 radical bc that is also a part of nature
   const grasses = ["冫", "氵", "⺌", "丶", "⺍", "灬", "艹", "⺮"];
   const trees = ["冫", "氵", "木", "禾", "米"];
+  let treeIndex = 0;
 
-  function drawDot(x, y) {
+  function drawGrass(x, y) {
     const dot = document.createElement("span");
+    dot.className = "grass";
     dot.innerText = grasses[getRandomInt(0, grasses.length)]; // 
     dot.style.top = y-7+"px";
     dot.style.left = x-7+"px";
     container.appendChild(dot);
   }
 
+  function drawTree() {
+    const treeSpans = Array.from(document.getElementsByClassName("tree"));
+    if (treeSpans.length > 15) {
+      const tree = treeSpans[treeIndex];
+      const lastTree = treeSpans[(treeIndex*15)%treeSpans.length]; // let's say every tree should consist of ~15 elems
+      tree.style.top = +(lastTree.style.top.split("px")[0]) + getRandomInt(1, 10) + "px";
+      tree.style.left = +(lastTree.style.left.split("px")[0]) + getRandomInt(0, 5) + "px";
+      tree.innerText = trees[getRandomInt(0, trees.length)];
+      treeIndex += 1;
+    }
+  }
+
   function removeGrass() {
-    const grasses = Array.from(document.getElementsByTagName("span"));
-    if (grasses.length > 250) grasses[0].remove();
+    const grassSpans = Array.from(document.getElementsByClassName("grass"));
+    if (grassSpans.length > 175) { 
+      let grass = grassSpans[0];
+      grass.className = "tree";
+      grass.innerText = "";
+    }
   }
 
   function handleTouch(e, isMobile = false) {
     e.preventDefault();
     let [x, y] = (!isMobile) ? [e.pageX, e.pageY] : [e.targetTouches[0].pageX, e.targetTouches[0].pageY];
-    drawDot(x, y);
+    drawGrass(x, y);
     let getX = (s=0,r) => x + r * Math.sin(s);
     let getY = (c=0,r) => y + r * Math.cos(c);
     let radius = 2;
@@ -38,8 +56,9 @@ window.addEventListener("load", () => {
     let interval = setInterval(() => {
       if (Math.random()>0.45) return; // skips sometimes
       radius = getRandomInt(10, 50);
-      drawDot(getX(s+=1,radius), getY(c+=1,radius));
+      drawGrass(getX(s+=1,radius), getY(c+=1,radius));
       removeGrass();
+      drawTree();
     }, 15);
     (!isMobile) ? onmouseup = () => {clearInterval(interval);} : ontouchend = () => {clearInterval(interval);};
   }
