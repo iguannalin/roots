cc76484e9a78e7b48f4d42a405602eaff1b52127
seasons986 <annalin12345@gmail.com>
2024-01-17 19:17:56 +0800

    cleanup but not quite where i want yet--elements still increase a lot

--- index.js
+++ index.js
@@ -11,12 +11,9 @@ window.addEventListener("load", () => {
   // 2) radicals that look like grass or trees
   // 3) water radicals bc water makes up everything
   // 4) fire 灬 radical bc that is also a part of nature
-  const grasses = ["冫", "氵", "⺌", "丶", "⺍", "灬", "艹", "⺮"];
-  const trees = ["冫", "氵", "木", "禾"];
+  const grasses = ["⺌", "丶", "⺍", "灬", "艹", "⺮"];//, "冫", "氵"];
+  const trees = ["木", "禾"];//, "冫", "氵"];
   let treeIndex = 0;
-  let isTouching = false;
-  let timer = 0;
-
   function drawGrass(x, y, isCenter) {
     const dot = document.createElement("span");
     dot.className = isCenter ? "treecenter" : "grass";
@@ -29,17 +26,16 @@ window.addEventListener("load", () => {
   function drawTree() {
     if (Math.random()>0.6) return; // skips sometimes
     const treeSpans = Array.from(document.getElementsByClassName("tree"));
-    if (treeSpans.length > 150) { // TODO differentiate time from tree count
+    if (treeSpans.length > 150) {
       const treeCenterSpans = Array.from(document.getElementsByClassName("treecenter"));
       const treeCenterIndex = Math.floor(treeIndex/25)%treeCenterSpans.length;
-      // console.log("length: ", treeCenterSpans.length," index: ", treeCenterIndex, " trees ", treeSpans.length);
       const centerTree = treeCenterSpans[treeCenterIndex];
       const tree = treeSpans[0];
       tree.style.top = +(centerTree.style.top.split("px")[0]) - getRandomInt(10,150) + "px";
       tree.style.left = +(centerTree.style.left.split("px")[0]) + ((Math.random()>0.5?-1:1) * getRandomInt(1,10)) + "px";
       tree.innerText = trees[getRandomInt(0, trees.length)];
       centerTree.innerText = trees[getRandomInt(0, trees.length)];
-      centerTree.className += " old";
+      centerTree.className = "treecenter old";
       if (tree.className=="tree") {
         tree.className = "old";
         treeIndex += 1;
@@ -71,19 +67,15 @@ window.addEventListener("load", () => {
     let s = 0; let c = 0;
     let interval = setInterval(() => {
       if (Math.random()>0.45) return; // skips sometimes
-      // isTouching = true;
-      // timer++;
       radius = getRandomInt(10, 50);
       drawGrass(getX(s+=1,radius), getY(c+=1,radius));
-      // if (timer%5==0) 
       removeGrass();
+      console.log(document.getElementsByTagName("span"));
     }, 15);
     (!isMobile) ? onmouseup = () => {clearInterval(interval);} : ontouchend = () => {clearInterval(interval);};
   }
 
   document.addEventListener('mousedown', handleTouch, {passive: false});
   document.addEventListener('touchstart', (e) => handleTouch(e, true), {passive: false});
-  // document.addEventListener('mouseup', () => isTouching = false, {passive: false});
-  // document.addEventListener('touchend', () => isTouching = false, {passive: false});
   // ontouchstart = (e) => handleTouch(e, true); // doesn't work if you want to get rid of auto text-select on safari (via e.preventDefault())
 });
