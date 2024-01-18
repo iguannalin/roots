f257574549e4c0d811f2c7ed01d6f426702cbc76
seasons986 <annalin12345@gmail.com>
2024-01-18 22:31:17 +0800

    pretty close to what i wanted

--- index.js
+++ index.js
@@ -12,51 +12,55 @@ window.addEventListener("load", () => {
   // 3) water radicals bc water makes up everything
   // 4) fire 灬 radical bc that is also a part of nature
   const grasses = ["⺌", "丶", "⺍", "灬", "艹", "⺮"];//, "冫", "氵"];
-  const trees = ["木", "禾"];//, "冫", "氵"];
+  const touchPoints = [];
+  const trees = ["木", "林", "森"];//, "冫", "氵"];
+  let grassIndex = 0;
   let treeIndex = 0;
-  function drawGrass(x, y, isCenter) {
-    const dot = document.createElement("span");
-    dot.className = isCenter ? "treecenter" : "grass";
+  let threshold = 150;
+  let treeThreshold = 25;
+
+  let timeout = (dot) => dot.innerText = "";
+
+  function drawGrass(x, y) {
+    if (Math.random()>0.4) return; // skips sometimes
+    let dot;
+    const grassSpans = Array.from(document.getElementsByClassName("grass"));
+    if (grassSpans.length > threshold) { // only the threshold amount of grass spans allowed
+      dot = grassSpans[grassIndex];
+      dot.style.top = y-7+"px";
+      dot.style.left = x-7+"px";
+      grassIndex = grassIndex >= threshold ? 0 : grassIndex + 1;
+      if (Math.random()>0.4) setTimeout(drawTree, getRandomInt(2500, 7000));
+      // clearTimeout(timeout);
+    }
+    else {
+      dot = document.createElement("span");
+      dot.className = "grass";
+      dot.style.top = y-7+"px";
+      dot.style.left = x-7+"px";
+      container.appendChild(dot);
+      // if (Math.random()>0.2) 
+      // setTimeout(timeout(dot), getRandomInt(8000, 10000)); // disappear after a while
+    }
     dot.innerText = grasses[getRandomInt(0, grasses.length)]; // 
-    dot.style.top = y-7+"px";
-    dot.style.left = x-7+"px";
-    container.appendChild(dot);
   }
 
   function drawTree() {
-    if (Math.random()>0.6) return; // skips sometimes
-    const treeSpans = Array.from(document.getElementsByClassName("tree"));
-    if (treeSpans.length > 150) {
-      const treeCenterSpans = Array.from(document.getElementsByClassName("treecenter"));
-      const treeCenterIndex = Math.floor(treeIndex/25)%treeCenterSpans.length;
-      const centerTree = treeCenterSpans[treeCenterIndex];
-      const tree = treeSpans[0];
-      tree.style.top = +(centerTree.style.top.split("px")[0]) - getRandomInt(10,150) + "px";
-      tree.style.left = +(centerTree.style.left.split("px")[0]) + ((Math.random()>0.5?-1:1) * getRandomInt(1,10)) + "px";
-      tree.innerText = trees[getRandomInt(0, trees.length)];
-      centerTree.innerText = trees[getRandomInt(0, trees.length)];
-      centerTree.className = "treecenter old";
-      if (tree.className=="tree") {
-        tree.className = "old";
-        treeIndex += 1;
-      }
-    }
-    // if (treeSpans.length > 100) {
-    //   treeSpans[0].remove();
-    //   treeCenterIndex -= 1;
-    // }
+    // if (Math.random()>0.6) return; // skips sometimes
+    if (!touchPoints[treeIndex]) return;
+    const tree = document.createElement("span");
+    const center = touchPoints[treeIndex];
+    tree.className = "tree";
+    tree.innerText = trees[getRandomInt(0, trees.length)];
+    center.x = +(center.x) + ((Math.random()>0.5?-1:1) * getRandomInt(1,10));
+    tree.style.left = center.x + "px";
+    center.y -= 1+(Math.random()*8); 
+    tree.style.top = center.y + "px";
+    center.ct += 1;
+    container.appendChild(tree);
+    if (center.ct > treeThreshold && treeIndex < touchPoints.length) treeIndex += 1;
   }
-
-  function removeGrass() {
-    const grassSpans = Array.from(document.getElementsByClassName("grass"));
-    if (grassSpans.length > 150) { 
-      let grass = grassSpans[0];
-      grass.className = "tree";
-      grass.innerText = "";
-      // drawTree();
-    }
-  }
-
+  
   function handleTouch(e, isMobile = false) {
     e.preventDefault();
     let [x, y] = (!isMobile) ? [e.pageX, e.pageY] : [e.targetTouches[0].pageX, e.targetTouches[0].pageY];
@@ -65,13 +69,13 @@ window.addEventListener("load", () => {
     let getY = (c=0,r) => y + r * Math.cos(c);
     let radius = 2;
     let s = 0; let c = 0;
+    for (let i = 0; i < getRandomInt(2,5); i++) {
+      touchPoints.push({x:x+(i*Math.random()*25), y:y+(i*Math.random()*25), ct: 0});
+    }
     let interval = setInterval(() => {
-      if (Math.random()>0.45) return; // skips sometimes
       radius = getRandomInt(10, 50);
       drawGrass(getX(s+=1,radius), getY(c+=1,radius));
-      removeGrass();
-      console.log(document.getElementsByTagName("span"));
-    }, 15);
+    }, 50);
     (!isMobile) ? onmouseup = () => {clearInterval(interval);} : ontouchend = () => {clearInterval(interval);};
   }
 
